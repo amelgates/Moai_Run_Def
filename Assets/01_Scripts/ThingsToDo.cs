a991f0887b62b9e166c5648ea5708584aed1562d
suicidio <90414812+amelgates@users.noreply.github.com>
2022-05-27 12:04:55 -0400

    animacion responsiva

--- Assets/01_Scripts/ThingsToDo.cs
+++ Assets/01_Scripts/ThingsToDo.cs
@@ -1,3 +1,6 @@
 //hacer textura al suelo
 //hacer que se mueva el mapa com, o si fuera un spawner
 //hacer que el juego deje de moverse cuando el jugador muera o deje de apretar botones.
+//hacer que el moai tenga fuego en la velocidad 3 
+//hacer que los objetos cuando colisionen en el moai se derritan
+//hacer que los objetos retumben cuando el moai salte
