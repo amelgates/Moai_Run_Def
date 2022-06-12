33365c43c7ed6fd0637014e8c9bc309052c7b6c5
amelgates <mirrai2888@gmail.com>
2022-06-12 15:12:17 -0400

    Revert "audio y ingleses"
    This reverts commit c774855481e5061bfe4ea07c93c76ffb362ef704.

--- Assets/01_Scripts/GameManager.cs
+++ Assets/01_Scripts/GameManager.cs
@@ -8,19 +8,8 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }//como acceder y modificar los datos de esta clase
     //esto es un single
     //es el unico que existe para que unity lo reconozca 
-<<<<<<< Updated upstream
 
     public bool play;
-=======
-    public int score = 0;
-    public bool moving;
-    public GameObject textDisplay;
-    public GameObject textMenu;
-    public GameObject textWarning;
-    //play booleano para el game Over 
-    public bool play = false;
-    public GameObject data;
->>>>>>> Stashed changes
     private void Awake()
     {
         // Si existe una instancia, se destruye.
@@ -44,32 +33,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
         
-=======
-        //score se esta concatena a una cadena que es un espacio vacio
-        //la otra forma es score.ToString()
-        //textDisplay.GetComponent<TextMeshProUGUI>().text = score + "m";
-        textDisplay.GetComponent<TextMeshProUGUI>().text = score.ToString();
-        //concadenacion es diferente de la suma
-        if(moving == false)
-        {
-            textWarning.SetActive(true);
-            textWarning.GetComponent<TextMeshProUGUI>().text = "Necesitas moverte";
-        }
-        if(moving == true)
-        {
-            textWarning.SetActive(false);
-        }
-
-    }
-    public void incrementScore()
-    {
-        score = score + 10;
-    }
-    public void incrementGameSpeed(float speed)
-    {
-        Time.timeScale = speed;
->>>>>>> Stashed changes
     }
 }
--- Assets/01_Scripts/Spawner.cs
+++ Assets/01_Scripts/Spawner.cs
@@ -7,65 +7,17 @@ public class Spawner : MonoBehaviour
     private float speed = 0.5f;
 
 
-<<<<<<< Updated upstream
-=======
-    private void Start()
-    {
-          StartCoroutine(spawnObstaculeRoutine());
-    }
-    private void spawnObstacule()
-    {
-        Vector3 randomPosition = new Vector3(Random.Range(transform.position.x - 10, transform.position.x + 10),transform.position.y, transform.position.z);
-        int tipo = Random.Range(1, 4);
-        //debe ser 4 porque el ultimo es excluyente solo se da con enteros si trabajas con floats ambos parametros son incluyentes
-        //con enteros solo el segundo es exluyente
-        switch (tipo)
-            //recordar siempre tener uno por defecto esto es por norma
-            //digamos que se buguea el ramdom y genera 5  generando un error y como el error no se contemplo se buguea
-            //default hace como si el error ya se hubiera contemblado generando algo
-        {
-            case 1:
-                Instantiate(Obstacule1, randomPosition, Quaternion.identity);
-                break;
-            case 2:
-                Instantiate(Obstacule2, randomPosition, Quaternion.identity);
-                break;
-            default :
-                Instantiate(Obstacule3, randomPosition, Quaternion.identity);
-                break;
-        }
-    }
->>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< Updated upstream
         
-=======
-        //if (GameManager.Instance.moving == true)
-        //{
-        //    StartCoroutine(spawnObstaculeRoutine());
-        //}
-        //else if (!GameManager.Instance.moving)
-        //{
-        //    StopCoroutine(spawnObstaculeRoutine());
-        //}
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
         transform.Translate(Vector3.forward*speed *Time.deltaTime);
 
-=======
-        while (true)
-        {
-            yield return new WaitForSeconds(5);
-            spawnObstacule();
-        }
->>>>>>> Stashed changes
     }
 }
