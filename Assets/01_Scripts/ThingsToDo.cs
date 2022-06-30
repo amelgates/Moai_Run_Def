9b3578a66245c57f660022064c7d893f836c4a32
amelgates <mirrai2888@gmail.com>
2022-06-30 00:15:19 -0400

    boton back en select moai

--- Assets/01_Scripts/ThingsToDo.cs
+++ Assets/01_Scripts/ThingsToDo.cs
@@ -1,14 +1,4 @@
-
-
-
-
-//si llegas a 420 se activan los lentes
-//si llegas a mil se activa el gorro
-
-
-
-
-// ver si se puede hacer las skins
-// idea es que en play haya un canvas donde puedas cambiarle cosas al maoi y escoger entre 3 moai y luego recien empieze el juego
-//una escena para cada moai a la cual te llevan botones
-//al inicio indicar las teclas que debes apretar
+//algo para registar el score
+//que en la velocidad 3 te el triple de puntos
+//el audio esta fuerte
+//poner el back en seleccion de moai
